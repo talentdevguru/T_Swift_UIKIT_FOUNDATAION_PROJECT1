cec4cf590546c6432d62593788c4ff20c3387b1d
TalentDev Guru <talent.dev.guru@gmail.com>
2019-10-22 23:25:37 +0900

    Initial

---
+++ T_SWIFT_UIKIT_FOUNDATION_PROJECT1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  T_SWIFT_UIKIT_FOUNDATION_PROJECT1
+//
+//  Created by TalentDevGuru on 10/22/19.
+//  Copyright © 2019 TalentDevGuru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
